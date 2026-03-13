b229fb2c5d403d83808d9b29bb090c503fa7b545
aditya-bhanushali <aditya.bhanushali24@sakec.ac.in>
2026-03-14 03:21:23 +0530

    implemented alerts

--- server/src/index.js
+++ server/src/index.js
@@ -19,6 +19,7 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/complaints', require('./routes/complaints'));
+app.use('/api/alerts', require('./routes/alerts'));
 
 // Basic Route
 app.get('/health', (req, res) => {
